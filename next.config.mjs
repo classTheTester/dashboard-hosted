cbcca32dacf406891c35ae14376c0fb9887dd977
eweisberg <71267834+classTheTester@users.noreply.github.com>
2025-06-12 03:28:41 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,12 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      '/graphs': { page: '/graphs' },
+    }
+  },
 }
 
 export default nextConfig
